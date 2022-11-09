477c8932f73032b1893038b368932e7edfec9427
Ralphvandodewaard <rtvd@ziggo.nl>
2022-11-10 00:54:07 +0100

    Improved projects descriptions

--- src/assets/projects.ts
+++ src/assets/projects.ts
@@ -3,35 +3,35 @@ import Project from '@/models/Project';
 const projects: Project[] = [
   {
     label: 'Urfbuilds',
-    description: 'Urfbuilds (2021) is a website where you can find and submit champion builds for the URF game mode.',
+    description: 'Urfbuilds (2021) is a website where you can discover and submit champion builds for the League of Legends game mode URF.',
     links: [
-      { label: 'Visit', url: 'https://urfbuilds.com/' }
+      { label: 'Visit', url: 'https://urfbuilds.com' }
     ]
   },
   {
     label: 'League-ranked',
-    description: 'League-ranked (2022) is a personal website where we track the match history and various stats of our ranked team.',
+    description: 'League-ranked (2022) is a personal website where we track the match history and various stats of our ranked team in League of Legends.',
     links: [
       { label: 'Visit', url: 'https://projectralph.com/league-ranked' }
     ]
   },
   {
     label: 'League-quotes',
-    description: 'League-quotes (2022) is a website where you have to guess which champion says a certain quote.',
+    description: 'League-quotes (2022) is a browser game where you have to guess as many times in a row as possible by which League of Legends champion a certain quote is said.',
     links: [
       { label: 'Visit', url: 'https://projectralph.com/league-quotes' }
     ]
   },
   {
     label: 'League-abilities',
-    description: 'League-abilities (2021) is a website where you have to guess which champion a certain ability belongs to.',
+    description: 'League-abilities (2021) is a browser game where you have to guess which League of Legends champion a certain ability belongs to.',
     links: [
       { label: 'Visit', url: 'https://projectralph.com/league-trivia' }
     ]
   },
   {
     label: 'Pokemon-binder',
-    description: 'Pokemon-binder (2022) is a website where you can view and filter card sets in various binder sizes.',
+    description: 'Pokemon-binder (2022) is a website where you can view and filter Pokemon TCG card sets and track your card collections in various binder sizes.',
     links: [
       { label: 'Visit', url: 'https://projectralph.com/pokemon-binder' }
     ]
@@ -58,7 +58,7 @@ const projects: Project[] = [
   },
   {
     label: 'VR Detective Training',
-    description: 'VR Detective Training (2020) is a virtual reality training built in collaboration with the Politie Amsterdam, with the goal of giving police detectives the opportunity to virtually practice working at crime scenes.',
+    description: 'VR Detective Training (2020) is a virtual reality concept game built in collaboration with the Politie Amsterdam, with the goal of giving police detectives the opportunity to virtually practice working at crime scenes.',
     links: []
   },
   {
