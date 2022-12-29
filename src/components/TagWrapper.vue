744680a642162e87dbc6f6d5268241ebc2bb093b
Ralphvandodewaard <rtvd@ziggo.nl>
2022-12-29 20:14:18 +0100

    Added tag colors

--- src/components/TagWrapper.vue
+++ src/components/TagWrapper.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="w-fit px-3 bg-green-300 text-neutral-950 text-xs rounded">
+  <div
+    :class="getTagColors"
+    class="w-fit px-2 text-xs rounded"
+  >
     {{ tag }}
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, computed } from 'vue';
 
 export default defineComponent({
   name: 'TagWrapper',
@@ -14,6 +17,37 @@ export default defineComponent({
       type: String,
       required: true
     }
+  },
+  setup(props) {
+    const getTagColors = computed<string>(() => {
+      const tag = props.tag.toLowerCase();
+
+      if (tag.includes('angular')) {
+        return 'bg-angular text-neutral-100';
+      } else if (tag.includes('vue')) {
+        return 'bg-vue text-neutral-950';
+      } else if (tag.includes('typescript')) {
+        return 'bg-typescript text-neutral-100';
+      } else if (tag.includes('tailwind')) {
+        return 'bg-tailwind text-neutral-950';
+      } else if (tag.includes('pokemon')) {
+        return 'bg-pokemon text-neutral-100';
+      } else if (tag.includes('riot')) {
+        return 'bg-riot text-neutral-100';
+      } else if (tag.includes('php')) {
+        return 'bg-php text-neutral-100';
+      } else if (tag.includes('mysql')) {
+        return 'bg-mysql text-neutral-100';
+      } else if (tag.includes('c#')) {
+        return 'bg-csharp text-neutral-100';
+      }
+
+      return 'bg-black text-neutral-100';
+    });
+
+    return {
+      getTagColors
+    };
   }
 });
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  safelist: [{ pattern: /bg-(angular|vue|typescript|tailwind|pokemon|riot|php|mysql|csharp)/ }],
   theme: {
     extend: {
       screens: {
@@ -15,7 +16,16 @@ module.exports = {
         neutral: {
           850: '#202020',
           950: '#0F0F0F'
-        }
+        },
+        angular: '#DD0031',
+        vue: '#42B883',
+        typescript: '#3178C6',
+        tailwind: '#0EA5E9',
+        pokemon: '#524DD1',
+        riot: '#DC291E',
+        php: '#4F5B93',
+        mysql: '#3E6E93',
+        csharp: '#953DAC'
       },
       width: {
         90: '22.5rem'
