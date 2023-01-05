d8610fc3f4c3eddc5138316a5c9a23599acd2709
Ralphvandodewaard <rtvd@ziggo.nl>
2023-01-05 18:30:40 +0100

    Removed projects file and added getting projects via json file

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,7 +10,7 @@
     />
     <div class="flex flex-wrap justify-center mobile:justify-start gap-8">
       <ContentWrapper
-        v-for="project in orderedProjects"
+        v-for="project in projectsReversed"
         :key="project.label"
         :label="project.label"
         :description="project.description"
@@ -24,12 +24,14 @@
 <script lang="ts">
 import {
   defineComponent,
+  Ref,
   ref,
+  computed,
   onMounted,
   onUnmounted
 } from 'vue';
+import axios from 'axios';
 import ContentWrapper from '@/components/ContentWrapper.vue';
-import projects from '@/assets/projects';
 import Link from '@/models/Link';
 import Project from '@/models/Project';
 
@@ -39,13 +41,17 @@ export default defineComponent({
     ContentWrapper
   },
   setup() {
+    const projects: Ref<Project[]> = ref([]);
+
+    const projectsReversed = computed<Project[]>(() => {
+      return [...projects.value].reverse();
+    });
+
     const personalLinks: Link[] = [
       { label: 'LinkedIn', url: 'https://www.linkedin.com/in/ralphvandodewaard/' },
       { label: 'Github', url: 'https://github.com/Ralphvandodewaard' },
       { label: 'Resume', url: '' }
     ];
-
-    const orderedProjects: Project[] = projects.reverse();
 
     const contentMargin = ref('');
 
@@ -53,9 +59,12 @@ export default defineComponent({
       contentMargin.value = `${(window.innerHeight / 2) - 192}px`;
     }
 
-    onMounted(() => {
+    onMounted(async() => {
       window.addEventListener('resize', setContentMargin);
       setContentMargin();
+
+      const response = await axios.get('https://projectralph.com/files/getprojects.php');
+      projects.value = response.data;
     });
 
     onUnmounted(() => {
@@ -63,8 +72,8 @@ export default defineComponent({
     });
 
     return {
+      projectsReversed,
       personalLinks,
-      orderedProjects,
       contentMargin
     };
   }
